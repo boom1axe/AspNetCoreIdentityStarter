1d1bf164c5e9d617a2ab8861951dbd132cd3832e
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-07-11 22:39:10 -0700

    Add project files.

---
+++ AspNetCoreIdentityStarter/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AspNetCoreIdentityStarter.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspNetCoreIdentityStarter.Areas.Identity.IdentityHostingStartup))]
+namespace AspNetCoreIdentityStarter.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
