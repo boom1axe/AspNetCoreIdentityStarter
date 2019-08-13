cf63a14a8782acfa50a59b57fa61b0cbb03be56b
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-08-12 22:16:35 -0700

    Create custom roles

---
+++ AspNetCoreIdentityStarter/Data/IdentityHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityStarter.Data
+{
+    internal static class IdentityHelper
+    {
+        internal static readonly string Admin = "Admin";
+        internal static readonly string Student = "Stu";
+
+        internal static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            IdentityResult roleResult;
+
+            foreach(string role in roles)
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        internal static void SetIdentityConfigOptions(IdentityOptions options)
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+        }
+    }
+}
